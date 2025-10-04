8679308b9da93a49d6e048ddad7dcee5700e1b2c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-04 10:11:41 +0530

    server file basic setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+//importing database connection
+const db = require("./db");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); //here it will convert the json string/data to js object and save it inside: req.body
+const PORT=process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port 3000");
+});
