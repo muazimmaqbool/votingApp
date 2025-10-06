3fab78ec324a59e5832c28787af3be47d67cc70d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-06 22:09:03 +0530

    issue fixed

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 //importing database connection
-// const db = require("./db");
+const db = require("./db");
+require("dotenv").config()
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //here it will convert the json string/data to js object and save it inside: req.body
 const PORT=process.env.PORT || 3000
 
 //importing router files
-const userRoutes=require("./Models/user")
+const userRoutes=require("./Routes/userRoutes")
 
 //using the routers
 app.use('/user',userRoutes)
